f012731617c86dad5e970bcfb730eaa49f9b985c
Kush Darji <kdarji1@student.gsu.edu>
2023-04-09 22:40:06 -0400

    completed hw 4

--- pl4330_hw4/tokenparser.py
+++ pl4330_hw4/tokenparser.py
@@ -12,3 +12,25 @@ tokens = [
     Token('SEMICOLON', ';'),
     Token('RBRACE', '}')
 ]
+
+ def tokenize(self):
+        tokens = []
+        regex_patterns = [
+            (r'➕', 'ADD'),
+            (r'➖', 'SUBTRACT'),
+            (r'✖️', 'MULTIPLY'),
+            (r'➗d', 'DIVIDE'),
+            (r'%', 'MODULO'),
+            (r'\(', 'LEFT_PAREN'),
+            (r'\)', 'RIGHT_PAREN'),
+            (r'→', 'ASSIGNMENT'),
+            (r'==', 'EQUALS'),
+            (r'<', 'LESS_THAN'),
+            (r'≤', 'LESS_THAN_EQUAL'),
+            (r'>', 'GREATER_THAN'),
+            (r'≥', 'GREATER_THAN_EQUAL'),
+            (r'&', 'LOGICAL_AND'),
+            (r'\|', 'LOGICAL_OR'),
+            (r'[_a-zA-Z][_a-zA-Z0-9]*', 'IDENTIFIER'),
+            (r'[-+]?[0-9]*\.?[0-9]+', 'NUMBER')
+        ]
