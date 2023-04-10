4225a7e57a0ee6238533b240a0055aabe1688e1e
Palpeleno <60727944+Palpeleno@users.noreply.github.com>
2023-04-10 18:07:34 -0400

    angd up and added more token like the math ones

--- pl4330_hw4/tokenparser.py
+++ pl4330_hw4/tokenparser.py
@@ -1,36 +1,34 @@
-tokens = [
-    Token('IF', 'if'),
-    Token('LPAREN', '('),
-    Token('ID', 'x'),
-    Token('GT', '>'),
-    Token('INT_LIT','0'),
-    Token('RPAREN', ')'),
-    Token('LBRACE','{'),
-    Token('ID', 'x'),
-    Token('ASSIGN', '='),
-    Token('ID', 'y'),
-    Token('SEMICOLON', ';'),
-    Token('RBRACE', '}')
-]
+def tokenize(self):
 
- def tokenize(self):
-        tokens = []
-        regex_patterns = [
-            (r'➕', 'ADD'),
-            (r'➖', 'SUBTRACT'),
-            (r'✖️', 'MULTIPLY'),
-            (r'➗d', 'DIVIDE'),
-            (r'%', 'MODULO'),
-            (r'\(', 'LEFT_PAREN'),
-            (r'\)', 'RIGHT_PAREN'),
-            (r'→', 'ASSIGNMENT'),
-            (r'==', 'EQUALS'),
-            (r'<', 'LESS_THAN'),
-            (r'≤', 'LESS_THAN_EQUAL'),
-            (r'>', 'GREATER_THAN'),
-            (r'≥', 'GREATER_THAN_EQUAL'),
-            (r'&', 'LOGICAL_AND'),
-            (r'\|', 'LOGICAL_OR'),
-            (r'[_a-zA-Z][_a-zA-Z0-9]*', 'IDENTIFIER'),
-            (r'[-+]?[0-9]*\.?[0-9]+', 'NUMBER')
-        ]
+    tokens = [
+        Token('IF', 'if'),
+        Token('LPAREN', '('),
+        Token('ID', 'x'),
+        Token('GT', '>'),
+        Token('INT_LIT','0'),
+        Token('RPAREN', ')'),
+        Token('LBRACE','{'),
+        Token('ID', 'x'),
+        Token('ASSIGN', '='),
+        Token('ID', 'y'),
+        Token('SEMICOLON', ';'),
+        Token('RBRACE', '}')
+        Token('➖', 'SUBTRACT'),
+        Token('➕', 'ADD'),
+        Token('✖️', 'MULTIPLY'),
+        Token('➗d', 'DIVIDE'),
+        Token('%', 'MODULO'),
+        Token('\(', 'LEFT_PAREN'),
+        Token('\)', 'RIGHT_PAREN'),
+        Token('→', 'ASSIGNMENT'),
+        Token('==', 'EQUALS'),
+        Token('<', 'LESS_THAN'),
+        Token('≤', 'LESS_THAN_EQUAL'),
+        Token('>', 'GREATER_THAN'),
+        Token('≥', 'GREATER_THAN_EQUAL'),
+        Token('&', 'LOGICAL_AND'),
+        Token('\|', 'LOGICAL_OR'),
+        Token('[_a-zA-Z][_a-zA-Z0-9]*', 'IDENTIFIER'),
+        Token('[-+]?[0-9]*\.?[0-9]+', 'NUMBER')
+    ]
+
