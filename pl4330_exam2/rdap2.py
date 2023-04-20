1071e70b1bb7da7facba264de5614456a1e573ea
Kush Darji <kdarji1@student.gsu.edu>
2023-04-20 19:34:53 -0400

    redoing some stmt functions with better syntax still looks wrong

--- pl4330_exam2/rdap2.py
+++ pl4330_exam2/rdap2.py
@@ -1,94 +1,105 @@
+        
 class Parser:
     def __init__(self, tokens):
         self.tokens = tokens
-        self.current_token = tokens[0]
         self.index = 0
 
     def parse(self):
         self.stmt()
-        if self.current_token != '$':
+        if self.index != '$': #end of file 
             raise Exception('parsing Error')        
 
+    # def peek(self):
+    #     if self.index < len(self.tokens):
+    #         return self.tokens[self.index]
+        
+    def match(self, expected_token):
+        if self.tokens[self.index] == expected_token:
+            self.index += 1
+        else:
+            raise Exception(f"Expected {expected_token}, got {self.tokens[self.index]}")    
+
+    def stmt(self):
+            if self.tokens[self.index] == 'if':
+                return self.if_stmt()
+            elif self.peek() == '{':
+                return self.block()
+            elif self.peek() == 'DataType':
+                return self.declare()
+            elif self.peek() == 'while':
+                return self.while_loop()
+            else:
+                return self.assign()
+    
     def stmt_list(self):
         self.stmt()
         while self.current_token == ';':
             self.match(';')
             self.stmt()
-
-    def stmt(self):
-        if self.peek() == 'if':
-            return self.if_stmt()
-        elif self.peek() == '{':
-            return self.block()
-        elif self.peek() == 'DataType':
-            return self.declare()
-        elif self.peek() == 'while':
-            return self.while_loop()
-        else:
-            return self.assign()
     
     def if_stmt(self):
-        self.eat('if')
-        self.eat('(')
+        self.match('if')
+        self.match('(')
         bool_expr = self.bool_expr()
-        self.eat(')')
+        self.match(')')
         if_block = self.block()
         else_block = None
-        if self.peek() == 'else':
-            self.eat('else')
+        if self.tokens[self.index] == 'else':
+            self.match('else')
             else_block = self.block()
         return ('if', bool_expr, if_block, else_block)
 
     def block(self):
-        self.eat('{')
+        self.match('{')
         stmt_list = self.stmt_list()
         return ('block', stmt_list)
     
     def declare(self):
-        self.eat('DataType')
-        id_handful = [self.eat_id()]
-        while self.peek() == ',':
-            self.eat(',')
-            id_handful.append(self.eat_id())
-        return ('deckare', id_handful)
+        self.match('DataType')
+        id_handful = self.match(self.tokens[self.index])
+        while self.tokens[self.index] == ',':
+            self.match(',')
+            id_handful
+        return ('declare', id_handful)
 
-    def assign(self):
-        id = self.eat_id()
-        self.eat('=')
+#here we are returning function statement of the next valid token, but 
+# is the return statments necessary or can seld.(statment type) be like that stright up 
+    def assign(self):                           
+        id = self.match(self.tokens[self.index])
+        self.match('=')
         expr = self.expr()
-        return ('assign', id, expr)
+        return ('assign', id, expr)                 #<--- necessary ?
 
     def expr(self):
-        term = self.term()
-        while self.peek() in ('+','-','*','/','%'):
-            operator = self.eat()
-            term2 = self.term()
-            term = (operator, term, term2)
-        return term
+        self.term()
+        while self.tokens[self.index] in ['+','-','*','/','%']:
+            self.match(self.tokens[self.index])
+            self.term()
     
     def term(self):
-        fact = self.fact()
-        while self.peek() in ('*','/','%'):
-            operator = self.eat()
-            fact2 = self.fact()
-            fact = (operator, fact2, fact)
-        return fact
+        self.fact()
+        while self.tokens[self.index] in ('*','/','%'):
+            self.match(self.tokens[self.index])
+            self.fact()
+            
+        
 
     def fact(self):
         if self.peek_type() == 'ID':
-            return self.eat_id()
+            return self.match_id()
         elif self.peek_type() in ('INT_LIT', 'FLOAT_LIT'):
-            return self.eat()
+            return self.match()
+        elif self.tokens[self.index] == '(':
+            self.match('(')
+            self.expr()
+            self.match(')')
         else:
-            self.eat('(')
-            expr = self.expr()
-            self.eat(')')
-            return expr
+            raise Exception(f"Invalid factor: {self.tokens[self.index]}")
 
     def bool_expr(self):
         bterm = self.bterm()
         while self.peek() in ('>','<','>=', '<='):
-            operator = self.eat()
+            operator = self.match()
             bterm2 = self.bterm()
             bterm = (operator, bterm, bterm2)
         return bterm
@@ -96,7 +107,7 @@ class Parser:
     def bterm(self):
         booleanAnd = self.booleanAnd()
         while self.peek() in ('==', '!='):
-            operator = self.eat()
+            operator = self.match()
             booleanAnd2= self.booleanAnd()
             booleanAnd = (operator, booleanAnd, booleanAnd2)
         return booleanAnd
@@ -104,7 +115,7 @@ class Parser:
     def booleanAnd(self):
         booleanOr = self.booleanOr()
         while self.peek() == '&&':
-            self.eat('&&')
+            self.match('&&')
             booleanOr2 = self.booleanOr()
             booleanOr = ('&&', booleanOr, booleanOr2)
         return booleanOr
@@ -112,7 +123,7 @@ class Parser:
     def booleanOr(self):
         expr = self.expr()
         while self.peek() == '||':
-            self.eat('||')
+            self.match('||')
             expr2 = self.expr()
  
 
